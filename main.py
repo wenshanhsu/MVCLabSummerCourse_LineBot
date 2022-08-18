2b2622a3f7a099495c2c709342e0577432d888ef
wenshanhsu <you@example.com>
2022-08-18 12:38:50 +0800

    main revise

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-'''
-This is my LineBot API
-How to Start:
-    > Step 0. Go to ./MVCLab-Summer-Course/LineBot/
-        > cd ./MVCLab-Summer-Course/LineBot
-    > Step 1. Install Python Packages
-        > pip install -r requirements.txt
-    > Step 2. Run main.py
-        > python main.py
-Reference:
-1. LineBot API for Python
-    > https://github.com/line/line-bot-sdk-python
-2. Pokemon's reference
-    > https://pokemondb.net/pokedex/all
-3. Line Developer Messaging API Doc
-    > https://developers.line.biz/en/docs/messaging-api/
-'''
 import os
 import re
 import json
